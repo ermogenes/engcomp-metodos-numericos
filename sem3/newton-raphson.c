b205c04c32f5e685be48feb6deb48985fbc43db1
ermogenes <ermogenes@gmail.com>
2015-10-25 08:31:49 -0200

    Tabs convertidos para 4 espaços

--- sem3/newton-raphson.c
+++ sem3/newton-raphson.c
@@ -1,58 +1,58 @@
 /*
-ImplementaÃ§Ã£o do mÃ©todo de Newton-Raphson
-para a aproximaÃ§Ã£o de raÃ­zes de funÃ§Ãµes reais.
+Implementação do método de Newton-Raphson
+para a aproximação de raízes de funções reais.
 
 Author: Ermogenes Palacio
 Date: 2015-10-22
 */
 
-#include <stdio.h>   // funÃ§Ãµes de IO
+#include <stdio.h>   // funções de IO
 #include <stdbool.h> // tipo booleano
-#include <math.h>    // funÃ§Ãµes matemÃ¡ticas
+#include <math.h>    // funções matemáticas
 
-const int    MAX_ITER = 200; // nÃºmero mÃ¡ximo de iteraÃ§Ãµes
-const double ERROR_FLAG = 999999; // flag que indica que nÃ£o convergiu
-const double PADRAO_EPSILON = 1e-14; // mÃ¡ximo denominador desejado
-const double PADRAO_TOLERANCIA = 1e-6; // maior erro aceitÃ¡vel
+const int    MAX_ITER = 200; // número máximo de iterações
+const double ERROR_FLAG = 999999; // flag que indica que não convergiu
+const double PADRAO_EPSILON = 1e-14; // máximo denominador desejado
+const double PADRAO_TOLERANCIA = 1e-6; // maior erro aceitável
 
 /* f(x) */
 double f(double x){
-	return pow(x, 3);
+    return pow(x, 3);
 }
 
 /* f'(x) */
 double f_linha(double x){
-	return 3 * pow(x, 2);
+    return 3 * pow(x, 2);
 }
 
-/* CÃ¡lculo das raÃ­zes */
+/* Cálculo das raízes */
 double newtonRaphson(double x, double epsilon_f, double epsilon_x){
     /*
     double x: valor inicial
-    double epsilon_f: menor denominador aceitÃ¡vel
-    double epsilon_x: maior erro aceitÃ¡vel
+    double epsilon_f: menor denominador aceitável
+    double epsilon_x: maior erro aceitável
     */
 
-    int i; // contador de iteraÃ§Ãµes
+    int i; // contador de iterações
 
-    double x0 = x; // aproximaÃ§Ã£o atual de x
+    double x0 = x; // aproximação atual de x
     double y; // f(x)
     double y_linha; // f'(x)
-    double x1; // prÃ³xima aproximaÃ§Ã£o de x
+    double x1; // próxima aproximação de x
     double erro_x; // erro atual
     double y_linha_abs; // magnitude do denominador atual
 
-    bool convergiu = false; // flag para controle de convergÃªncia
+    bool convergiu = false; // flag para controle de convergência
 
-    printf("## MÃ©todo de Newton-Raphson:\n\n");
-    printf("FunÃ§Ã£o: `x^3`\n\n");
-    printf("### ITERAÃ‡Ã•ES:\n\n");
+    printf("## Método de Newton-Raphson:\n\n");
+    printf("Função: `x^3`\n\n");
+    printf("### ITERAÇÕES:\n\n");
     printf("`x = %.2f`\t`e_f = %.1e`\t`e_y = %.1e`\n\n", x, epsilon_f, epsilon_x);
 
     printf("`i`|`x`|`f(x)`|`f'(x)`|Erro\n");
     printf("---|---|---|---|---\n");
 
-    // iterando atÃ© o mÃ¡ximo permitido
+    // iterando até o máximo permitido
     for(i = 1; i <= MAX_ITER; i++){
 
         // calcula y e y' no ponto atual
@@ -64,28 +64,28 @@ double newtonRaphson(double x, double epsilon_f, double epsilon_x){
 
         printf("`%d`|`%.4f`|`%.8f`|`%.4f`|`%.1e`\n", i, x0, y, y_linha, y_linha_abs);
 
-        // atingiu o menor denominador aceitÃ¡vel entÃ£o nÃ£o converge
+        // atingiu o menor denominador aceitável então não converge
         if(y_linha_abs < epsilon_f){
             break;
         }
 
-        // calcula a prÃ³xima aproximaÃ§Ã£o de x
+        // calcula a próxima aproximação de x
         x1 = x0 - y / y_linha;
 
-        // calcula o erro da aproximaÃ§Ã£o
+        // calcula o erro da aproximação
         erro_x = fabs(x1 - x0);
 
-        // se erro menor que a tolerÃ¢ncia, convergiu
+        // se erro menor que a tolerância, convergiu
         if(erro_x < epsilon_x){
             convergiu = true;
             break;
         }
 
-        // assume a prÃ³xima aproximaÃ§Ã£o como a atual
+        // assume a próxima aproximação como a atual
         x0 = x1;
-	}
+    }
 
-    // usa cÃ³digo de erro para caso de nÃ£o convergÃªncia
+    // usa código de erro para caso de não convergência
     if(!convergiu){
         x1 = ERROR_FLAG;
     }
@@ -102,7 +102,7 @@ int main()
         printf("Nao convergiu.");
     }
     else{
-        printf("\nAproximaÃ§Ã£o encontrada: `%.5f`", result);
+        printf("\nAproximação encontrada: `%.5f`", result);
     }
     return 0;
 }
